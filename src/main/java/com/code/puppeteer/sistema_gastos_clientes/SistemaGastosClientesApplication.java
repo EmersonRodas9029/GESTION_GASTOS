bdcfccdf7a3b653c9f93857fc9fb83b7669aff2b
ORSTED9029 <theripper446@gmail.com>
2025-06-10 18:01:53 -0600

    Configuración inicial del entorno, proyecto Spring Boot y conexión a base de datos MySQL"

---
+++ src/main/java/com/code/puppeteer/sistema_gastos_clientes/SistemaGastosClientesApplication.java
@@ -0,0 +1,13 @@
+package com.code.puppeteer.sistema_gastos_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGastosClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGastosClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/puppeteer/sistema_gastos_clientes/SistemaGastosClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.puppeteer.sistema_gastos_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGastosClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
